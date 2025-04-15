13f4c64a4f4a35faf5bbaa4ee2a7269adbcc324f
Ishant1910 <ishantnandeshwar2005@gmail.com>
2025-04-15 11:43:59 +0530

    first commit

---
+++ t.cpp
@@ -0,0 +1,12 @@
+// calculator.c
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a = 2, b = 3;
+    printf("Addition: %d\n", a + b);// a+b
+    printf("Exponent: %.0f\n", pow(a, b));  // 2^3 = 8
+
+
+    return 0;
+}  
