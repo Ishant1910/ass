11720dc85b5c6fc6cab61852d5fdd4bca3d2ad5c
Ishant1910 <ishantnandeshwar2005@gmail.com>
2025-04-15 11:45:02 +0530

    second commit

--- t.cpp
+++ t.cpp
@@ -7,6 +7,10 @@ int main() {
     printf("Addition: %d\n", a + b);// a+b
     printf("Exponent: %.0f\n", pow(a, b));  // 2^3 = 8
 
+        // Hexadecimal display
+    printf("Value of a in hex: %X\n", a);      // Hex of 2 = 2
+    printf("Value of b in hex: %X\n", b);      // Hex of 3 = 3
+    printf("Addition in hex: %X\n", a + b);    // Hex of 5 = 5
 
     return 0;
 }  
